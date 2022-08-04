13adb4f40cc0d2add189b176e32fe3620102144a
Darryl Brooks <darryl.brooks@aa.com>
2022-08-04 12:15:12 -0500

    refactor: prettier

--- pages/room/[name].tsx
+++ pages/room/[name].tsx
@@ -18,15 +18,16 @@ export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
 const ChatRoom = (props: { roomName: string }) => {
   return (
     <div className="flex justify-center items-center h-screen min-w-7xl p-3">
-      <div className="flex flex-col h-36 bg-red-500">
-        <p className="text-white text-xl">
+      <div className="flex flex-col p-20">
+        <p className="text-white text-xl pb-5">
           You made it to: {props.roomName} chat room
         </p>
-        {/* <div className=" bg-white rounded-lg"> */}
-        <div className="flex items-end p-3">
-          <input type="text" className="w-full border-2 rounded-md" />
-          <button className="p-3 rounded-md bg-white">Send</button>
-          {/* </div> */}
+        <div className="py-10 bg-white rounded-lg">
+          <p>Messages</p>
+        </div>
+        <div className="flex items-end p-3 mx-auto">
+          <input type="text" className="p-2 mr-3 border-2 rounded-md" />
+          <button className="p-2 rounded-md bg-white">Send</button>
         </div>
       </div>
     </div>
