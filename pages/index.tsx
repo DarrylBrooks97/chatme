22c7db0516330555a8d75096727e4fad54bbe2e8
Darryl Brooks <darryl.brooks@aa.com>
2022-08-04 11:14:12 -0500

    fix: fixing tailwind config

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Index: NextPage = () => {
   return (
     <div>
-      <p>Hello World</p>
+      <p className="text-white">Hello World</p>
     </div>
   );
 };
