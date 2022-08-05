701784c95c5422ce6de01e2141a0a308fd1194cc
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-05 17:59:57 -0500

    chore: updating for SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,32 @@ import type { AppProps } from 'next/app';
 import { UserProvider } from '@supabase/auth-helpers-react';
 import { supabaseClient } from '@supabase/auth-helpers-nextjs';
 import Layout from '@components/Layout';
+import { DefaultSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider supabaseClient={supabaseClient}>
+      <DefaultSeo
+        title="Chatme 👋🏾"
+        openGraph={{
+          type: 'website',
+          url: 'https://chatme-xi.vercel.app/',
+          site_name: 'Chatme 👋🏾',
+          description: 'A quick chat app to talk to friends, family, and randoms in real time.',
+          images: [
+            {
+              url: 'https://chatme-xi.vercel.app//SEO.png',
+              width: 800,
+              height: 450,
+              alt: 'Chatme',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@apxflex',
+          cardType: 'summary_large_image',
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
