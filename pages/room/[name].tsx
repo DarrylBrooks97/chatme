df435cc94567b75f6af4ba8e9035cfeabd59370a
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-05 09:59:35 -0500

    feat: adding back button and styling

--- pages/room/[name].tsx
+++ pages/room/[name].tsx
@@ -59,17 +59,24 @@ const ChatRoom = (props: { roomName: string }) => {
 
   return (
     <div className="flex justify-center items-center h-screen min-w-7xl p-3">
-      <div className="flex flex-col w-4/5 min-w-2xl">
+      <div className="flex flex-col w-full md:w-4/5 max-w-xl">
+        <button
+          className="absolute top-2 left-2 rounded-md bg-white px-3 py-2"
+          tabIndex={1}
+          onClick={() => (window.location.href = '/')}
+        >
+          Back
+        </button>
         <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-blue-500 text-xl pb-5 text-center">
           {props.roomName} chat room
         </p>
         <div className="py-10 bg-white rounded-lg">
           {messages?.map(message => (
-            <div className="flex p-3 border-b-2 ">
+            <div className="flex p-3 border-b-2" key={message.time}>
               <div className="rounded-full overflow-hidden">
                 <img
                   src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
-                  alt={`avatar with ${message}`}
+                  alt={`avatar @ ${message.time}`}
                   className="w-[50px] h-[50px] "
                 />
               </div>
@@ -90,7 +97,7 @@ const ChatRoom = (props: { roomName: string }) => {
             value={newMessage}
             onChange={e => setNewMessage(e.currentTarget.value)}
           />
-          <button className="p-2 rounded-md bg-white" onClick={addMessage}>
+          <button className="p-2 rounded-md bg-green-700" onClick={addMessage}>
             Send
           </button>
         </div>
