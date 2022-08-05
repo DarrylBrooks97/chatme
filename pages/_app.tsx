ef00bf68d1c3785e404cbc25cf8fbcf27231146b
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-05 17:47:34 -0500

    feat: adding supabase stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { UserProvider } from '@supabase/auth-helpers-react';
+import { supabaseClient } from '@supabase/auth-helpers-nextjs';
+import Layout from '@components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider supabaseClient={supabaseClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
