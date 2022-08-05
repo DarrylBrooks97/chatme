3ede1943866fe473bcf318d740dc9b129157d42f
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-05 09:49:52 -0500

    refactor: prettifying

--- pages/api/chat/add.ts
+++ pages/api/chat/add.ts
@@ -1,34 +1,35 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import Pusher from "pusher";
+import { NextApiRequest, NextApiResponse } from 'next';
+import Pusher from 'pusher';
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    const {body, method} = req;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { body, method } = req;
 
-    if(method !== "POST") {
-        res.status(405).json({error: "Method not allowed"});
-        return;
-    }
-    
-    const pusher = new Pusher({
-        appId: process.env.PUSHER_APP_ID as string,
-        key: process.env.PUSHER_API_KEY as string,
-        secret: process.env.PUSHER_API_KEY_SECRET as string,
-        cluster: process.env.PUSHER_CLUSTER as string,
-    });
-    
-    const { roomName, message} = body;
-    
-    const r = await pusher.trigger(roomName, "new-message", {
-        message,
-    });
-    
-    if(!r.ok){
-        res.status(400).json({
-            message: "Message failed sending",
-        });
-    }
+  if (method !== 'POST') {
+    res.status(405).json({ error: 'Method not allowed' });
+    return;
+  }
 
-    res.status(200).json({
-        message: "Message sent",
+  const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID as string,
+    key: process.env.PUSHER_API_KEY as string,
+    secret: process.env.PUSHER_API_KEY_SECRET as string,
+    cluster: process.env.PUSHER_CLUSTER as string,
+  });
+
+  const { roomName, message } = body;
+
+  const r = await pusher.trigger(roomName, 'new-message', {
+    message,
+    time: new Date().toISOString(),
+  });
+
+  if (!r.ok) {
+    res.status(400).json({
+      message: 'Message failed sending',
     });
+  }
+
+  res.status(200).json({
+    message: 'Message sent',
+  });
 }
