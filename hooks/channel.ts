c904cc4250060fba7fcfad71cee03440fe1a1432
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-06 16:04:07 -0500

    feat: getting online member count

--- hooks/channel.ts
+++ hooks/channel.ts
@@ -1,7 +1,15 @@
 import { pusher } from '@clients/pusher';
 
+const getOnlineCount = (channel: any) => {
+  channel.bind('pusher:subscription_count', (data: { subscription_count: number }) => {
+    document.getElementById('online')!.innerHTML = `${data.subscription_count} online!`;
+  });
+};
+
 const joinRoom = (channelName: string, fn: (t: any) => void) => {
   const channel = pusher.subscribe(`cache-${channelName}`);
+  getOnlineCount(channel);
+
   return channel.bind('new-message', (data: any) => {
     fn(data);
   });
--- pages/room/[name].tsx
+++ pages/room/[name].tsx
@@ -70,62 +70,58 @@ const ChatRoom = (props: { user: User; roomName: string }) => {
       setMessages(prevMessages => [...prevMessages, data]);
       setNewMessage('');
     });
-    console.log({ roomClient });
 
     return () => {
       leaveRoom(roomName);
     };
   }, []);
-  // console.log({ s: roomClient });
 
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex flex-col">
-        <p className=" font-semibold text-3xl text-transparent bg-clip-text animate-lightSpeed bg-gradient-to-r from-cyan-500 via-pink-500 to-cyan-500 bg-[length:300%_100%] pb-5 text-center">
-          Welcome to {roomName} room !
-        </p>
-        <div className="flex">
-          <div className="h-[10px] w-[10px] bg-green-300 rounded-full animate-ping self-center" />
-          <p className="text-white ml-3">{roomClient?.subscriptionCount} online</p>
-        </div>
-        <div className=" bg-white rounded-lg overflow-y-scroll h-96">
-          {messages?.map(message => (
-            <div className="flex p-3 border-b-2" key={message.time}>
-              <div className="rounded-full h-[50px] overflow-hidden">
-                <img
-                  src={message.avatar}
-                  alt={`avatar @ ${message.time}`}
-                  className="w-[50px] h-[50px]"
-                  onClick={() =>
-                    window.open(
-                      `https://twitter.com/${user.user_metadata['preferred_username']}`,
-                      '_blank',
-                    )
-                  }
-                />
-              </div>
-              <div className="w-full break-words self-center">
-                <p className="text-sm text-purple-500 ml-3">@{message.userName}</p>
-                <p className="text-gray-800 text-sm  ml-3">{message.message}</p>
-                <time className="text-xs text-gray-500 float-right">
-                  {formatDistanceToNow(new Date(message.time))} ago
-                </time>
-              </div>
+    <div className="flex flex-col p-3">
+      <p className="break-words font-semibold text-3xl text-transparent bg-clip-text animate-lightSpeed bg-gradient-to-r from-cyan-500 via-pink-500 to-cyan-500 bg-[length:650%_100%] pb-5 text-center">
+        Welcome to {roomName} room !
+      </p>
+      <div className="flex">
+        <div className="h-[10px] w-[10px] bg-green-300 rounded-full animate-ping self-center" />
+        <p className="text-white ml-3" id="online"></p>
+      </div>
+      <div className=" bg-white rounded-lg overflow-y-scroll h-96 w-full">
+        {messages?.map(message => (
+          <div className="flex p-3 border-b-2" key={message.time}>
+            <div className="rounded-full h-[60px] w-[60px] overflow-hidden">
+              <img
+                src={message.avatar}
+                alt={`avatar @ ${message.time}`}
+                className="w-full h-full"
+                onClick={() =>
+                  window.open(
+                    `https://twitter.com/${user.user_metadata['preferred_username']}`,
+                    '_blank',
+                  )
+                }
+              />
             </div>
-          ))}
-        </div>
-        <div className="flex items-end p-3 mx-auto">
-          <input
-            type="text"
-            className="p-2 mr-3 border-2 w-full mx-auto rounded-md bg-white text-black"
-            onKeyDown={handleEnter}
-            value={newMessage}
-            onChange={e => setNewMessage(e.currentTarget.value)}
-          />
-          <button className="p-2 rounded-md bg-green-700 text-white" onClick={addMessage}>
-            Send
-          </button>
-        </div>
+            <div className="w-full break-words self-center">
+              <p className="text-sm text-purple-500 ml-3">@{message.userName}</p>
+              <p className="text-gray-800 text-sm  ml-3">{message.message}</p>
+              <time className="text-xs text-gray-500 float-right">
+                {formatDistanceToNow(new Date(message.time))} ago
+              </time>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex items-end p-3 mx-auto">
+        <input
+          type="text"
+          className="p-2 mr-3 border-2 w-full mx-auto rounded-md bg-white text-black"
+          onKeyDown={handleEnter}
+          value={newMessage}
+          onChange={e => setNewMessage(e.currentTarget.value)}
+        />
+        <button className="p-2 rounded-md bg-green-700 text-white" onClick={addMessage}>
+          Send
+        </button>
       </div>
     </div>
   );
