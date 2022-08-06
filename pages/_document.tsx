715d34caed9d8c5e78105795ad8e66aadf905f70
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-06 18:41:35 -0500

    feat: only logging prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,14 @@ const Document = () => {
   return (
     <Html>
       <Head>
-        <script
-          async
-          defer
-          data-website-id="aaf9d810-79a4-410d-870a-d8cb447c9fd7"
-          src="https://metabird-umami.vercel.app/umami.js"
-        ></script>
+        {process.env.NODE_ENV === 'production' && (
+          <script
+            async
+            defer
+            data-website-id="aaf9d810-79a4-410d-870a-d8cb447c9fd7"
+            src="https://metabird-umami.vercel.app/umami.js"
+          ></script>
+        )}
       </Head>
       <body>
         <Main />
