6407f0cb680bf729bdcb796f12c363a3d7e7686e
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-06 14:46:37 -0500

    chore: adding tracking

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="aaf9d810-79a4-410d-870a-d8cb447c9fd7"
+          src="https://metabird-umami.vercel.app/umami.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
