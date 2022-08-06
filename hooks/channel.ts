9a850b487e4c456bc1c17b60f2792dca22c326f2
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-05 19:37:21 -0500

    feat: making presence channel

--- hooks/channel.ts
+++ hooks/channel.ts
@@ -1,14 +1,15 @@
 import { pusher } from '@clients/pusher';
+import { PresenceChannel } from 'pusher-js';
 
 const joinRoom = (channelName: string, fn: (t: any) => void) => {
-  const channel = pusher.subscribe(`cache-${channelName}`);
+  const channel = pusher.subscribe(`presence-cache-${channelName}`);
   return channel.bind('new-message', (data: any) => {
     fn(data);
-  });
+  }) as PresenceChannel;
 };
 
 const leaveRoom = (channelName: string) => {
-  pusher.unsubscribe(`cache-${channelName}`);
+  pusher.unsubscribe(`presence-cache-${channelName}`);
 };
 
 export { joinRoom, leaveRoom };
