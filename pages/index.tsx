679303dbf1fec8743c5be0679109fc0d43232908
Darryl Brooks <darryl.brooks@aa.com>
2022-08-09 16:17:02 -0500

    chore: setting up for joinable channels

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,29 @@ import { useUser } from '@supabase/auth-helpers-react';
 import { login } from 'hooks/supbase';
 import { useEffect } from 'react';
 
+interface Room {
+  name: string;
+  memberCount: number;
+}
+const openRooms: Room[] = [
+  // {
+  //   name: 'general',
+  //   memberCount: 2,
+  // },
+  // {
+  //   name: 'Darryl',
+  //   memberCount: 200,
+  // },
+  // {
+  //   name: 'chas',
+  //   memberCount: 320,
+  // },
+  // {
+  //   name: 'the last slimetto',
+  //   memberCount: 2,
+  // },
+];
+
 const Index: NextPage = () => {
   const { user } = useUser();
 
@@ -38,7 +61,7 @@ const Index: NextPage = () => {
         A quick chat app to talk to friends, family, and randoms in real time.
       </h2>
       <div className="flex flex-col text-center p-3">
-        <p className="text-white text-xl p-3">Enter a room name to chat in !</p>
+        <p className="text-white text-xl p-3">Enter a room name !</p>
         <form action="POST" onSubmit={submitRoom}>
           <input name="roomName" className="rounded-md p-3 mr-3 bg-white text-black" />
           <button
@@ -55,13 +78,28 @@ const Index: NextPage = () => {
           Signin
         </button>
       </div>
-      {/* <div className="flex flex-col">
-        {channels?.map((channel: any) => (
-          <div className="flex p-3 border-b-2" key={channel.name}>
-            <div className="flex-1">
-              <p className="text-sm text-gray-600">{channel.name}</p>
+      <div className="flex justify-around items-center p-3">
+        <div className="bg-white w-full h-0.5"></div>
+        <p className="text-white text-2xl px-4">Or</p>
+        <div className="bg-white w-full h-0.5"></div>
+      </div>
+      <p className="text-slate-300 text-2xl pt-4">Join a room</p>
+      {openRooms.length < 1 && (
+        <p className="text-white text-lg md:text-2xl p-3">No available rooms 🙁. Go start one!</p>
+      )}
+      {/* <div className="grid grid-cols-2 gap-4 p-3 grow">
+        {openRooms?.map((room: Room) => (
+          <NextLink href={`http://localhost:3000/room/${room.name}`}>
+            <div className="rounded-md border-2 border-slate-400 py-16 relative" key={room.name}>
+              <div className="flex-1">
+                <p className="text-xl text-white">{room.name}</p>
+              </div>
+              <div className="flex justify-around items-center absolute bottom-3 right-3">
+                <div className="rounded-full mr-3 w-3 h-3  animate-ping bg-green-500"></div>
+                <p className="text-white">Online: {room.memberCount}</p>
+              </div>
             </div>
-          </div>
+          </NextLink>
         ))}
       </div> */}
     </div>
